a4b9f36122dac3ffc9e780aad59fc13be77e1397
Hyein93 <dlsl5418@gmail.com>
2024-07-05 17:54:57 +0900

    0705

---
+++ 90_test/자바스크립트/script/script.js
@@ -0,0 +1,63 @@
+// 요소 찾기
+const btn1 = document.querySelectorAll('#tabs li').item(0);
+const btn2 = document.querySelectorAll('#tabs li').item(1);
+const btn3 = document.querySelectorAll('#tabs li').item(2);
+const btn4 = document.querySelectorAll('#tabs li').item(3);
+const btn5 = document.querySelectorAll('#tabs li').item(4);
+
+const cont1 = document.querySelector('#tab1');
+const cont2 = document.querySelector('#tab2');
+const cont3 = document.querySelector('#tab3');
+const cont4 = document.querySelector('#tab4');
+const cont5 = document.querySelector('#tab5');
+
+btn1.onclick = function () {
+    resetF();
+    resetC();
+    btn1.classList.add('text');
+    cont1.classList.add('on');
+}
+
+
+btn2.onclick = function () {
+    resetF();
+    resetC();
+    btn2.classList.add('text');
+    cont2.classList.add('on');
+}
+
+btn3.onclick = function () {
+    resetF();
+    resetC();
+    btn3.classList.add('text');
+    cont3.classList.add('on');
+}
+
+btn4.onclick = function () {
+    resetF();
+    resetC();
+    btn4.classList.add('text');
+    cont4.classList.add('on');
+}
+
+btn5.onclick = function () {
+    resetF();
+    resetC();
+    btn5.classList.add('text');
+    cont5.classList.add('on');
+}
+
+function resetF() {
+    cont1.classList.remove('on');
+    cont2.classList.remove('on');
+    cont3.classList.remove('on');
+    cont4.classList.remove('on');
+    cont5.classList.remove('on');
+}
+function resetC() {
+    btn1.classList.remove('text');
+    btn2.classList.remove('text');
+    btn3.classList.remove('text');
+    btn4.classList.remove('text');
+    btn5.classList.remove('text');
+}
